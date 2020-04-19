21c07de7faf37f360df85ed644d8a7dc548836ef
Paolo D'Elia <paolo.delia99@gmail.com>
2020-04-19 17:12:42 +0200

    See if it works with this config

--- test/browser-test-config/base-karma.js
+++ test/browser-test-config/base-karma.js
@@ -25,6 +25,8 @@ module.exports = function (config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
+    failOnEmptyTestSuite: false, // so npm doesn't fail
+
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
